f992ab37010e8e967a6463797ecbed0c10380eb9
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2023-01-18 22:42:35 -0300

    health-statistics second interaction, tuple structs?

--- rust/health-statistics/src/lib.rs
+++ rust/health-statistics/src/lib.rs
@@ -1,35 +1,27 @@
-pub struct User {
-    name: String,
-    age: u32,
-    weight: f32,
-}
+pub struct User(String, u32, f32);
 
 impl User {
     pub fn new(name: String, age: u32, weight: f32) -> Self {
-        User {
-            name: String::from(name),
-            age: age as u32, 
-            weight: weight as f32
-    }
+        Self(name, age, weight)
 }
 
     pub fn name(&self) -> &str {
-        &self.name
+        &self.0
     }
 
     pub fn age(&self) -> u32 {
-        self.age
+        self.1
     }
 
     pub fn weight(&self) -> f32 {
-        self.weight
+        self.2
     }
 
     pub fn set_age(&mut self, new_age: u32) {
-        self.age = new_age
+        self.1 = new_age
     }
 
     pub fn set_weight(&mut self, new_weight: f32) {
-        self.weight = new_weight
+        self.2 = new_weight
     }
 }
