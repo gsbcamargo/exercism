b2384a38fac915a236920be37bdf6eeffe0840eb
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2023-02-03 21:06:07 -0300

    changed module attributes to the same name as the parameters

--- elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -1,23 +1,23 @@
 defmodule KitchenCalculator do
 
   # in milliliters
-  @us_cup 240
-  @us_fluid_ounce 30
-  @us_teaspoon 5
-  @us_tablespoon 15
+  @cup 240
+  @fluid_ounce 30
+  @teaspoon 5
+  @tablespoon 15
 
   def get_volume({_, volume}), do: volume
 
-  def to_milliliter({:cup, volume}), do: {:milliliter, volume * @us_cup}
-  def to_milliliter({:fluid_ounce, volume}), do: {:milliliter, volume * @us_fluid_ounce}
-  def to_milliliter({:teaspoon, volume}), do: {:milliliter, volume * @us_teaspoon}
-  def to_milliliter({:tablespoon, volume}), do: {:milliliter, volume * @us_tablespoon}
+  def to_milliliter({:cup, volume}), do: {:milliliter, volume * @cup}
+  def to_milliliter({:fluid_ounce, volume}), do: {:milliliter, volume * @fluid_ounce}
+  def to_milliliter({:teaspoon, volume}), do: {:milliliter, volume * @teaspoon}
+  def to_milliliter({:tablespoon, volume}), do: {:milliliter, volume * @tablespoon}
   def to_milliliter(volume_pair), do: volume_pair
 
-  def from_milliliter({_, volume}, :cup), do: {:cup, volume / @us_cup}
-  def from_milliliter({_, volume}, :fluid_ounce), do: {:fluid_ounce, volume / @us_fluid_ounce}
-  def from_milliliter({_, volume}, :teaspoon), do: {:teaspoon, volume / @us_teaspoon}
-  def from_milliliter({_, volume}, :tablespoon), do: {:tablespoon, volume / @us_tablespoon}
+  def from_milliliter({_, volume}, :cup), do: {:cup, volume / @cup}
+  def from_milliliter({_, volume}, :fluid_ounce), do: {:fluid_ounce, volume / @fluid_ounce}
+  def from_milliliter({_, volume}, :teaspoon), do: {:teaspoon, volume / @teaspoon}
+  def from_milliliter({_, volume}, :tablespoon), do: {:tablespoon, volume / @tablespoon}
   def from_milliliter(volume_pair, :milliliter), do: volume_pair
 
   def convert(volume_pair, unit), do: volume_pair |> to_milliliter |> from_milliliter(unit)
